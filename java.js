5425b426840e2dea8d3602334320698d764137f6
juliavanleeuwen <78502866+juliavanleeuwen@users.noreply.github.com>
2021-08-20 20:08:26 -0600

    button scroll effect

--- java.js
+++ java.js
@@ -1,12 +1,79 @@
 
+/*get started page */
 
   $("#historyBtn").click(function() {
     $('html, body').animate({
         scrollTop: $(".history").offset().top
-    }, 2000);
+    }, 1000);
+}); 
+
+$("#needBtn").click(function() {
+  $('html, body').animate({
+      scrollTop: $(".whatNeed").offset().top
+  }, 1000);
+});
+
+$("#techBtn").click(function() {
+  $('html, body').animate({
+      scrollTop: $(".technique").offset().top
+  }, 1000);
+});
+
+$("#introBtn").click(function() {
+  $('html, body').animate({
+      scrollTop: $(".introFolds").offset().top
+  }, 1000);
+});
+
+$("#introVidBtn").click(function() {
+  $('html, body').animate({
+      scrollTop: $(".videoTut").offset().top
+  }, 1000);
+});
+
+/* tips and tricks */
+$("#begTips").click(function() {
+  $('html, body').animate({
+      scrollTop: $(".tips").offset().top
+  }, 1000);
+});
+
+$("#advidBtn").click(function() {
+  $('html, body').animate({
+      scrollTop: $(".videoTut").offset().top
+  }, 1000);
+});
+
+$("#advOrigami").click(function() {
+  $('html, body').animate({
+      scrollTop: $(".advTech").offset().top
+  }, 1000);
+});
+
+$("#typeOrigami").click(function() {
+  $('html, body').animate({
+      scrollTop: $(".types").offset().top
+  }, 1000);
 });
 
 
+/* related hobbies page */
 
+$("#paperBtn").click(function() {
+  $('html, body').animate({
+      scrollTop: $(".paperHobbies").offset().top
+  }, 1000);
+});
 
+$("#japanBtn").click(function() {
+  $('html, body').animate({
+      scrollTop: $(".japanHobbies").offset().top
+  }, 1000);
+});
+
+$("#handsBtn").click(function() {
+  $('html, body').animate({
+      scrollTop: $(".handHobbies").offset().top
+  }, 1000);
+});
 
